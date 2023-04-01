29d4d8cd9d505f27adb504589b030ad8b316b33f
dreamxian4 <122443241+dreamxian4@users.noreply.github.com>
2023-04-01 12:50:22 +0800

    linux服务器

---
+++ 服务端/include/clogic.h
@@ -0,0 +1,37 @@
+#ifndef CLOGIC_H
+#define CLOGIC_H
+
+#include"TCPKernel.h"
+
+class CLogic
+{
+public:
+    CLogic( TcpKernel* pkernel )
+    {
+        m_pKernel = pkernel;
+        m_sql = pkernel->m_sql;
+        m_tcp = pkernel->m_tcp;
+    }
+public:
+    //设置协议映射
+    void setNetPackMap();
+    /************** 发送数据*********************/
+    void SendData( sock_fd clientfd, char*szbuf, int nlen )
+    {
+        m_pKernel->SendData( clientfd ,szbuf , nlen );
+    }
+    /************** 网络处理 *********************/
+    //注册
+    void RegisterRq(sock_fd clientfd, char*szbuf, int nlen);
+    //登录
+    void LoginRq(sock_fd clientfd, char*szbuf, int nlen);
+
+    /*******************************************/
+
+private:
+    TcpKernel* m_pKernel;
+    CMysql * m_sql;
+    Block_Epoll_Net * m_tcp;
+};
+
+#endif // CLOGIC_H
---
+++ 服务端/src/clogic.cpp
@@ -0,0 +1,24 @@
+#include "clogic.h"
+
+void CLogic::setNetPackMap()
+{
+    NetPackMap(DEF_PACK_REGISTER_RQ)    = &CLogic::RegisterRq;
+    NetPackMap(DEF_PACK_LOGIN_RQ)       = &CLogic::LoginRq;
+}
+
+//注册
+void CLogic::RegisterRq(sock_fd clientfd,char* szbuf,int nlen)
+{
+    printf("clientfd:%d RegisterRq\n", clientfd);
+   
+}
+
+//登录
+void CLogic::LoginRq(sock_fd clientfd ,char* szbuf,int nlen)
+{
+    printf("clientfd:%d LoginRq\n", clientfd);
+
+    STRU_LOGIN_RS rs;
+    rs.m_lResult = password_error;
+    SendData( clientfd , (char*)&rs , sizeof rs );
+}
