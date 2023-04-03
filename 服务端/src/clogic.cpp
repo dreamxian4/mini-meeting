a06938f5217d7bf0b4c285bd46cf08b4aa42303b
dreamxian4 <122443241+dreamxian4@users.noreply.github.com>
2023-04-03 15:23:43 +0800

    linux服务端

--- 服务端/src/clogic.cpp
+++ 服务端/src/clogic.cpp
@@ -10,7 +10,27 @@ void CLogic::setNetPackMap()
 void CLogic::RegisterRq(sock_fd clientfd,char* szbuf,int nlen)
 {
     printf("clientfd:%d RegisterRq\n", clientfd);
-   
+    //拆包
+    STRU_REGISTER_RQ *rq=(STRU_REGISTER_RQ*)szbuf;
+
+    //查数据库，有没有号码，
+    list<string>lstRes;
+    char strsql[1024];
+    sprintf(strsql,"select tel from t_user where tel='%s'",rq->m_tel);
+    m_sql->SelectMysql(strsql,1,lstRes);
+
+    STRU_REGISTER_RS rs;
+    //如果有，已存在，如果没有，注册成功
+    if(lstRes.size()>0){
+        rs.m_lResult=tel_is_exist;
+    }else{
+        rs.m_lResult=register_success;
+        sprintf(strsql,"insert into t_user(tel,password,name,icon,feeling)values('%s','%s','%s',5,'happy')",rq->m_tel,rq->m_password,rq->m_tel);
+        if(!m_sql->UpdataMysql(strsql)){
+            printf("update fail:%s\n",strsql);
+        }
+    }
+    SendData(clientfd,(char*)&rs,sizeof(rs));
 }
 
 //登录
@@ -18,7 +38,7 @@ void CLogic::LoginRq(sock_fd clientfd ,char* szbuf,int nlen)
 {
     printf("clientfd:%d LoginRq\n", clientfd);
 
-    STRU_LOGIN_RS rs;
-    rs.m_lResult = password_error;
-    SendData( clientfd , (char*)&rs , sizeof rs );
+//    STRU_LOGIN_RS rs;
+//    rs.m_lResult = password_error;
+//    SendData( clientfd , (char*)&rs , sizeof rs );
 }
